51ed5dceb7c729718ad01dd993dc5d7dd2c3b211
Alina <alina@duolingo.com>
2025-10-04 18:06:12 -0400

    Add config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gallery-wall/',
+  base: process.env.NODE_ENV === 'production' ? '/gallery-wall/' : '/',
 })
